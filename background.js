a90e13b1ae5eedea23a11f757ff7eaa66eb6a1a1
VictorSarov <victor.sarov@rentalmatics.com>
2026-07-13 09:05:49 +0100

    all good

--- background.js
+++ background.js
@@ -1,6 +1,11 @@
 
-chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { message: 'sendMessage from background.js' }, (response) => {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status !== 'complete') return;
+    chrome.tabs.sendMessage(tabId, { message: 'sendMessage from background.js' }, (response) => {
         console.log('this is response from content script: ', response);
     });
 });
+
+// let a = chrome.runtime.getURL('assets/gear.png');
+
+// console.log(a);
--- content.js
+++ content.js
@@ -7,4 +7,30 @@
         console.log('sender: ', sender);
         sendResponse({ message: 'this is response from content.js' });
     });
+
+    // function makeDevSpace() {
+    //     const devSpaceDiv = document.createElement('div');
+    //     Object.assign(devSpaceDiv.style, {
+    //         position: 'fixed',
+    //         bottom: '0',
+    //         left: '0',
+    //         width: '100%',
+    //         background: 'gray',
+    //         color: 'black',
+    //         fontSize: '20px',
+    //         zIndex: '999999',
+    //         padding: '8px'
+    //     });
+
+    //     document.body.appendChild(devSpaceDiv);
+
+    //     const bookmarkBtn = document.createElement('img');
+    //     bookmarkBtn.src = chrome.runtime.getURL('assets/gear.png');
+    //     bookmarkBtn.className = 'ytp-button ' + 'bookmark-btn';
+    //     bookmarkBtn.title = 'Click to bookmark current timestamp';
+    //     bookmarkBtn.style.cssText = 'width: 40px; height: 40px; cursor: pointer; margin-right: 10px;';
+    //     devSpaceDiv.appendChild(bookmarkBtn);
+    // }
+
+    // makeDevSpace();
 })();
