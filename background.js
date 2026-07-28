06561e1f3585888f81ad956fb6a2522e91455435
VictorSarov <victor.sarov@rentalmatics.com>
2026-07-28 13:10:19 +0100

    all good

--- background.js
+++ background.js
@@ -4,20 +4,29 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     const isOnTimer = tab.url.includes('https://vclock.com/');
 
     if (isOnTimer) {
-        console.log('on timer skipped');
+        // console.log('on timer skipped');
         return;
     }
 
     if (changeInfo.status === 'complete' && tab.url.includes('youtube.com')) {
-        console.log('============================');
-        console.log('tabId:', tabId);
-        console.log('changeInfo:', changeInfo);
-        console.log('tab:', tab);
+        // console.log('============================');
+        // console.log('tabId:', tabId);
+        // console.log('changeInfo:', changeInfo);
+        // console.log('tab:', tab);
 
-        console.log('YouTube tab opened/loaded:', tab.url);
+        // console.log('YouTube tab opened/loaded:', tab.url);
+
+        console.log('tabs', chrome.tabs);
 
         chrome.tabs.sendMessage(tabId, {
             message: 'omg you opened youtube'
         });
     }
+
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+        console.log('============================');
+        console.log('message received:', message);
+        console.log('sender:', sender);
+        console.log('sendResponse:', sendResponse);
+    });
 });
--- content.js
+++ content.js
@@ -7,4 +7,17 @@
         console.log('sender:', sender);
         console.log('sendResponse:', sendResponse);
     });
+
+    chrome.runtime.sendMessage({
+        from: 'content.js',
+        message: 'content.js sent you message 1'
+    })
+
+    const url = chrome.runtime.getURL('assets/freeIcon.png');
+    console.log('url', url);
+
+
+    const tabs = chrome.tabs;
+    console.log('tabs', tabs);
+
 })();
