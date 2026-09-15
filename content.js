acf88d02acf2a03998157f13ef20ede77fa47a46
VictorSarov <victor.sarov@rentalmatics.com>
2026-09-15 11:52:41 +0100

    all good

--- content.js
+++ content.js
@@ -1,4 +1,15 @@
 (() => {
+    const isAllowed = window.location.href.includes('youtube.com');
+
+    if (!isAllowed) {
+        console.log('not allowed');
+        return;
+    }
+
+    let videoId;
+    let timeStampOfVideo;
+    let allBookMarks = [];
+
     // get all items
     chrome.storage.local.get(['foo', 'bar'], (result) => {
         console.log('result of storage', result);
@@ -20,7 +31,7 @@
         bottom: '0',
         left: '50%',
         transform: 'translateX(-50%)',
-        width: '800px',
+        width: '200px',
         height: '100px',
         border: '1px solid black',
         display: 'flex',
@@ -32,11 +43,53 @@
 
     document.body.appendChild(devDiv);
 
-    //  img
+    function imageClicked() {
+        console.log('image clicked');
+        console.log('videoId', videoId);
+        console.log('timeStampOfVideo', document.getElementsByClassName('video-stream')[0].currentTime);
 
+        const videoTimeStamp = document.getElementsByClassName('video-stream')[0].currentTime;
+
+        // check if previously exists
+        chrome.storage.local.get(videoId, (result) => {
+            console.log('result: ', result);
+
+            const isThereResult = result.length;
+            console.log('isThereResult: ', isThereResult);
+
+            const prevExists = Boolean(isThereResult);
+            console.log('prevExists: ', prevExists);
+
+            const arrayToAdd = result[videoId];
+            console.log('arrayToAdd: ', arrayToAdd);
+
+            if (prevExists) {
+                allBookMarks = [...arrayToAdd];
+            }
+
+            console.log('allBookMarks after transform', allBookMarks);
+
+            allBookMarks.push(videoTimeStamp);
+            allBookMarks.sort((a, b) => a - b);
+
+            let newVideoObj = {
+                [videoId]: allBookMarks
+            };
+            console.log('newVideoObj', newVideoObj);
+
+            chrome.storage.local.set(newVideoObj).then(() => {
+                chrome.storage.local.get(null, (result) => {
+                    console.log('final storage object after alteration', result);
+                });
+            });
+        });
+    }
+
+    //  img
     const myImg1 = document.createElement('img');
     myImg1.src = chrome.runtime.getURL('assets/img1.png');
     myImg1.title = 'click to add';
+    myImg1.addEventListener('click', imageClicked);
 
     Object.assign(myImg1.style, {
         height: '50px',
@@ -55,6 +108,26 @@
         console.log('message received:', message);
         console.log('sender:', sender);
         console.log('sendResponse:', sendResponse);
+
+        const isNewVideo = message.type === 'NEW_VIDEO';
+        // console.log('isNewVideo: ', isNewVideo);
+
+        const isNewTimeStamp = message.type === 'TIMESTAMP';
+        console.log("isNewTimeStamp: ", isNewTimeStamp);
+
+        if (isNewVideo) {
+            videoId = message.videoId;
+            timeStampOfVideo = document.getElementsByClassName('video-stream')[0];
+            // console.log('timeStampOfVideo: ', timeStampOfVideo);
+        }
+
+        if(isNewTimeStamp){
+            document.querySelector('video').currentTime = message.timestamp;
+            console.log('triggered');
+        }
+
+
+
     });
 
     const imgUrl = chrome.runtime.getURL('assets/sparta.png');
@@ -66,44 +139,44 @@
 
     // element after 5 seconds
 
-    let ytDiv = document.getElementById('start');
+    // let ytDiv = document.getElementById('start');
 
-    let redSquare = document.createElement('div');
+    // let redSquare = document.createElement('div');
 
-    function waitForElementToAppear(selector, callback) {
-        const allReadyExists = document.querySelector(selector);
-        console.log('allReadyExists: ', allReadyExists);
+    // function waitForElementToAppear(selector, callback) {
+    //     const allReadyExists = document.querySelector(selector);
+    //     console.log('allReadyExists: ', allReadyExists);
 
-        if (allReadyExists) {
-            callback(allReadyExists);
-            return;
-        }
+    //     if (allReadyExists) {
+    //         callback(allReadyExists);
+    //         return;
+    //     }
 
-        const observer = new MutationObserver(() => {
-            const elementThatIsDueToAppear = document.querySelector(selector);
+    //     const observer = new MutationObserver(() => {
+    //         const elementThatIsDueToAppear = document.querySelector(selector);
 
-            if (elementThatIsDueToAppear) {
-                observer.disconnect();
-                callback(elementThatIsDueToAppear);
-            }
-        });
+    //         if (elementThatIsDueToAppear) {
+    //             observer.disconnect();
+    //             callback(elementThatIsDueToAppear);
+    //         }
+    //     });
 
-        observer.observe(document.body, {
-            childList: true,
-            subtree: true
-        });
-    }
+    //     observer.observe(document.body, {
+    //         childList: true,
+    //         subtree: true
+    //     });
+    // }
 
-    waitForElementToAppear('#start', (element) => {
-        console.log('triggered');
-        Object.assign(redSquare.style, {
-            height: '100px',
-            width: '100px',
-            backgroundColor: 'red'
-        });
+    // waitForElementToAppear('#start', (element) => {
+    //     console.log('triggered');
+    //     Object.assign(redSquare.style, {
+    //         height: '100px',
+    //         width: '100px',
+    //         backgroundColor: 'red'
+    //     });
 
-        element.appendChild(redSquare);
-    });
+    //     element.appendChild(redSquare);
+    // });
 
-    // 
+    //
 })();
--- popup.js
+++ popup.js
@@ -1,24 +1,90 @@
 import { EXPORTED_STRING } from './constants.js';
-console.log('EXPORTED_STRING: ', EXPORTED_STRING);
+import { getCurrentTab } from './helper/getCurrentTab.js';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
     console.log('popup.js loaded');
 
-    let h1El = document.querySelector('h1');
-    h1El.style = 'color: red';
+    let currentTab = await getCurrentTab();
+    console.log('currentTab: ', currentTab);
 
-    // add items
+    if (Object.keys(currentTab).length && currentTab.url.includes('youtube.com')) {
+        console.log('on youtube');
 
-    const myBtn = document.getElementById('myBtn');
+        const url = new URL(currentTab.url);
+        const videoId = url.searchParams.get('v');
+        console.log('videoId: ', videoId);
 
-    myBtn.addEventListener('click', async () => {
-        chrome.storage.local.set({ foo: 'foo in storage', bar: 'bar in storage' });
-    });
+        // BUTTONS
 
-    // delete
-    const deleteBtn = document.getElementById('deleteBtn');
+                const mainUl = document.getElementById('mainUl');
+        console.log('mainUl: ', mainUl);
 
-    deleteBtn.addEventListener('click', async () => {
-        chrome.storage.local.remove('keyToDelete');
-    });
+        const clearAllBtnConst = document.getElementById('clearAllBtn');
+        clearAllBtnConst.addEventListener('click', () => {
+            chrome.storage.local.remove(videoId);
+            mainUl.innerHTML = 'no bookmarks';
+        });
+
+        const clearStorageConst = document.getElementById('clearStorage');
+        clearStorageConst.addEventListener('click', () => {
+            chrome.storage.local.clear();
+            mainUl.innerHTML = 'no bookmarks';
+        });
+
+        
+
+        // STUFF FOR UL AND LIST
+
+        // check if video has bookmarks
+
+        chrome.storage.local.get(videoId, (resultOfStorage) => {
+            console.log('resultOfStorage: ', resultOfStorage);
+
+            const prevExists = Object.keys(resultOfStorage).length;
+            console.log('prevExists: ', prevExists);
+
+            if (!prevExists) {
+                mainUl.innerHTML = 'no bookmarks';
+            } else {
+                let tsArray = resultOfStorage[videoId];
+
+                tsArray.forEach((timestamp) => {
+                    const li = document.createElement('li');
+
+                    li.textContent = timestamp;
+                    Object.assign(li.style, {
+                        marginTop: '10px',
+                        textDecoration: 'underline',
+                        cursor: 'pointer'
+                    });
+
+                    li.addEventListener('click', () => {
+                        console.log('Clicked timestamp:', timestamp);
+
+                        // move the video timestamp
+
+                        // chrome.scripting.executeScript({
+                        //     target: { tabId: currentTab.id},
+                        //     func: (secArg) => {
+                        //         document.querySelector('video').currentTime = secArg
+                        //     }  ,
+                        //     args: [timestamp]
+                        // });
+
+                        // Do whatever you want here
+
+                        console.log('message ready');
+
+                        chrome.tabs.sendMessage(currentTab.id, { type: 'TIMESTAMP', timestamp: timestamp });
+
+                        console.log('message sent');
+                    });
+
+                    mainUl.appendChild(li);
+                });
+            }
+        });
+
+        // on youtube end
+    }
 });
